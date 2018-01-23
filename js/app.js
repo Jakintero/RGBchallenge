95a5d94a8abdf98c2fa2d361aad092502ea527ee
Javier Quintero <jakintero@hotmail.com>
2018-01-23 16:16:05 -0500

    Haciendo el juego infinito

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
-var correct = Math.floor(Math.random() * 2);
+var correct;
 
-$('.option').on('click', function() {
-  var index = $('.option').index(this);
-  if (index == correct) {
-    alert("Muy bien!!");
-  } else {
-      alert("Re paila!");
+// Inicializa el juego
+game();
+
+// Enlaza el click de los círculos a la función guess
+$('.option').on('click', guess);
+
+function game() {
+  correct = Math.floor(Math.random() * 2);
+  $('.option').each(function(index) {
+
+
+    var color = generateColor();
+    $(this).css('background-color', color);
+
+    if (index == correct) {
+      $('.question').text(color);
+    }
+  });
+}
+
+function guess() {
+    var index = $('.option').index(this);
+    if (index == correct) {
+      alert("Muy bien!!");
+    } else {
+        alert("Re paila!");
+    }
+    game();
   }
-});
-
-$('.option').each(function(index) {
 
 
-  var color = generateColor();
-  $(this).css('background-color', color);
 
-  if (index == correct) {
-    $('.question').text(color);
-  }
-});
+
+
 
 function random() {
   return Math.floor(Math.random() * 255);
